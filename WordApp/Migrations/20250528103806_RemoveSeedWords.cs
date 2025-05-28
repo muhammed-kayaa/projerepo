904180b84f08157c5e3e1cb7b4351a14caf6357d
muhammed-kayaa <149070657+muhammed-kayaa@users.noreply.github.com>
2025-05-28 13:38:45 +0300

    1.1.6

---
+++ WordApp/Migrations/20250528103806_RemoveSeedWords.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WordApp.Migrations
+{
+    public partial class RemoveSeedWords : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -20);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -19);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -18);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -17);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -16);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -15);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -14);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -13);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -12);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -11);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -10);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -9);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -8);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -7);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -6);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -5);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -4);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -3);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -2);
+
+            migrationBuilder.DeleteData(
+                table: "Words",
+                keyColumn: "WordID",
+                keyValue: -1);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Words",
+                columns: new[] { "WordID", "CorrectStreak", "EngWordName", "IsKnown", "LastCorrectDate", "Picture", "RepeatCount", "TurWordName", "UserId" },
+                values: new object[,]
+                {
+                    { -20, 0, "notebook", false, null, "C:/words/notebook.jpg", 0, "defter", null },
+                    { -19, 0, "pencil", false, null, "C:/words/pencil.jpg", 0, "kurşun kalem", null },
+                    { -18, 0, "pen", false, null, "C:/words/pen.jpg", 0, "kalem", null },
+                    { -17, 0, "door", false, null, "C:/words/door.jpg", 0, "kapı", null },
+                    { -16, 0, "window", false, null, "C:/words/window.jpg", 0, "pencere", null },
+                    { -15, 0, "chair", false, null, "C:/words/chair.jpg", 0, "sandalye", null },
+                    { -14, 0, "table", false, null, "C:/words/table.jpg", 0, "masa", null },
+                    { -13, 0, "computer", false, null, "C:/words/computer.jpg", 0, "bilgisayar", null },
+                    { -12, 0, "phone", false, null, "C:/words/phone.jpg", 0, "telefon", null },
+                    { -11, 0, "star", false, null, "C:/words/star.jpg", 0, "yıldız", null },
+                    { -10, 0, "moon", false, null, "C:/words/moon.jpg", 0, "ay", null },
+                    { -9, 0, "sun", false, null, "C:/words/sun.jpg", 0, "güneş", null },
+                    { -8, 0, "water", false, null, "C:/words/water.jpg", 0, "su", null },
+                    { -7, 0, "tree", false, null, "C:/words/tree.jpg", 0, "ağaç", null },
+                    { -6, 0, "house", false, null, "C:/words/house.jpg", 0, "ev", null },
+                    { -5, 0, "car", false, null, "C:/words/car.jpg", 0, "araba", null },
+                    { -4, 0, "dog", false, null, "C:/words/dog.jpg", 0, "köpek", null },
+                    { -3, 0, "cat", false, null, "C:/words/cat.jpg", 0, "kedi", null },
+                    { -2, 0, "book", false, null, "C:/words/book.jpg", 0, "kitap", null },
+                    { -1, 0, "apple", false, null, "C:/words/apple.jpg", 0, "elma", null }
+                });
+        }
+    }
+}
--- WordApp/src/Data/AppDbContext.cs
+++ WordApp/src/Data/AppDbContext.cs
@@ -22,31 +22,8 @@ namespace WordApp.Data
             modelBuilder.Entity<Word>()
                 .HasMany(w => w.WordSamples)
                 .WithOne(ws => ws.Word)
-                .HasForeignKey(ws => ws.WordID);
-
-            // 20 kelime seed data (WordID negatif verildi)
-            modelBuilder.Entity<Word>().HasData(
-                new Word { WordID = -1, EngWordName = "apple", TurWordName = "elma", Picture = "C:/words/apple.jpg" },
-                new Word { WordID = -2, EngWordName = "book", TurWordName = "kitap", Picture = "C:/words/book.jpg" },
-                new Word { WordID = -3, EngWordName = "cat", TurWordName = "kedi", Picture = "C:/words/cat.jpg" },
-                new Word { WordID = -4, EngWordName = "dog", TurWordName = "köpek", Picture = "C:/words/dog.jpg" },
-                new Word { WordID = -5, EngWordName = "car", TurWordName = "araba", Picture = "C:/words/car.jpg" },
-                new Word { WordID = -6, EngWordName = "house", TurWordName = "ev", Picture = "C:/words/house.jpg" },
-                new Word { WordID = -7, EngWordName = "tree", TurWordName = "ağaç", Picture = "C:/words/tree.jpg" },
-                new Word { WordID = -8, EngWordName = "water", TurWordName = "su", Picture = "C:/words/water.jpg" },
-                new Word { WordID = -9, EngWordName = "sun", TurWordName = "güneş", Picture = "C:/words/sun.jpg" },
-                new Word { WordID = -10, EngWordName = "moon", TurWordName = "ay", Picture = "C:/words/moon.jpg" },
-                new Word { WordID = -11, EngWordName = "star", TurWordName = "yıldız", Picture = "C:/words/star.jpg" },
-                new Word { WordID = -12, EngWordName = "phone", TurWordName = "telefon", Picture = "C:/words/phone.jpg" },
-                new Word { WordID = -13, EngWordName = "computer", TurWordName = "bilgisayar", Picture = "C:/words/computer.jpg" },
-                new Word { WordID = -14, EngWordName = "table", TurWordName = "masa", Picture = "C:/words/table.jpg" },
-                new Word { WordID = -15, EngWordName = "chair", TurWordName = "sandalye", Picture = "C:/words/chair.jpg" },
-                new Word { WordID = -16, EngWordName = "window", TurWordName = "pencere", Picture = "C:/words/window.jpg" },
-                new Word { WordID = -17, EngWordName = "door", TurWordName = "kapı", Picture = "C:/words/door.jpg" },
-                new Word { WordID = -18, EngWordName = "pen", TurWordName = "kalem", Picture = "C:/words/pen.jpg" },
-                new Word { WordID = -19, EngWordName = "pencil", TurWordName = "kurşun kalem", Picture = "C:/words/pencil.jpg" },
-                new Word { WordID = -20, EngWordName = "notebook", TurWordName = "defter", Picture = "C:/words/notebook.jpg" }
-            );
+                .HasForeignKey(ws => ws.WordID);            // Seed kelime kaldırıldı, sadece uygulama üzerinden eklenenler olacak
+            // modelBuilder.Entity<Word>().HasData();
         }
     }
 }
